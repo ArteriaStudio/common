1e97d8fbb08d382e7e251da5afd59cab43e9caad
Rink <rink@mxd.mesh.ne.jp>
2025-01-05 18:44:44 +0900

    merger pass to path.

--- inc/libux/uxFormats.h
+++ inc/libux/uxFormats.h
@@ -29,4 +29,7 @@ struct	TBPolygons
 {
 	std::vector<TBVertex>	m_pVertices;	//　頂点データ
 	std::vector<uint16_t>	m_pIndices; 	//　索引データ
+
+	uint64_t	m_uInputLayoutId;		//　入力レイアウト識別子
+	uint32_t	m_uShaderId;			//　シェーダプログラム識別子
 };
