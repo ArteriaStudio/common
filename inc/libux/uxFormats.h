00a90856e3cf5ae4ab3bc35f7184f1e431d2e0d0
Rink <rink@mxd.mesh.ne.jp>
2025-01-06 00:50:26 +0900

    update

--- inc/libux/uxFormats.h
+++ inc/libux/uxFormats.h
@@ -30,6 +30,21 @@ struct	TBPolygons
 	std::vector<TBVertex>	m_pVertices;	//　頂点データ
 	std::vector<uint16_t>	m_pIndices; 	//　索引データ
 
-	uint64_t	m_uInputLayoutId;		//　入力レイアウト識別子
-	uint32_t	m_uShaderId;			//　シェーダプログラム識別子
+	uint64_t		m_uInputLayoutId;		//　入力レイアウト識別子
+	uint32_t		m_uShaderId;			//　シェーダプログラム識別子
+	VkIndexType 	m_vkIndexType;			//　索引配列要素サイズ
+};
+
+//　共通頂点バッファ
+struct	TPMXVertex
+{
+	XMFLOAT4	pos;
+	XMFLOAT4	normal;
+	XMFLOAT2	uv;
+	XMUINT4 	boneno;
+	XMFLOAT4	weight;
+	uint32_t	weightType; 	//　WEIGHTTYPE
+	XMFLOAT3	fSDEFC; 		//　SDEFC
+	XMFLOAT3	fSDEFR0;		//　SDEFR0
+	XMFLOAT3	fSDEFR1;		//　SDEFR1
 };
